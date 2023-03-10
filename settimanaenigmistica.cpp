138c1b6c7faf391d47f0480cd5b836b4da0c848f
GianMattiaPetroni <petronigianmattia@gmail.com>
2023-03-10 14:33:38 +0100

    settimanaenigmistica con diagonale

--- settimanaenigmistica.cpp
+++ settimanaenigmistica.cpp
@@ -4,99 +4,99 @@ using namespace std;
  
 int main()
 {
-    int rows = 10;
-    int columns = 10;
-    string words[] = {"ciao", "word", "apple", "barbabietole", "oshio"};
-    char empty = ' ';
+    int righe = 10;
+    int colonne = 10;
+    string parola[] = {"ciao", "word", "apple", "barbabietole", "oshio"};
+    char vuoto = ' ';
  
  
-    int wordsLength = sizeof(words) / sizeof(words[0]);
+    int parolaLength = sizeof(parola) / sizeof(parola[0]);
     int contatore = 0;
-    char matrix[rows][columns];
+    char matrix[righe][colonne];
  
     // fill matrix with ' '
-    for (int i = 0; i < rows; i++)
+    for (int i = 0; i < righe; i++)
     {
-        for (int j = 0; j < columns; j++)
+        for (int j = 0; j < colonne; j++)
         {
-            matrix[i][j] = empty;
+            matrix[i][j] = vuoto;
         }
     }
  
-    for (int index = 0; index < wordsLength; index++)
+    for (int indice = 0; indice < parolaLength; indice++)
     {
-        if (index % 3 == 0)
+        if (indice % 3 == 0)
         { // orizzontale
-            if (words[index].length() > columns)
+            if (parola[indice].length() > colonne)
             {
-                cout << "word " << words[index] << " is too long" << endl;
+                cout << "word " << parola[indice] << " is too long" << endl;
                 continue;
             }
             else
             {
                 contatore++;
-                int x = random() % rows; // row random
-                int y = random() % (columns - words[index].length());
+                int x = random() % righe; // row random
+                int y = random() % (colonne - parola[indice].length());
  
-                while (matrix[x][y] != empty)
+                while (matrix[x][y] != vuoto)
                 {
-                    x = random() % rows; // row random
-                    y = random() % (columns - words[index].length());
+                    x = random() % righe; // row random
+                    y = random() % (colonne - parola[indice].length());
                 }
  
-                for (int i = 0; i < words[index].length(); i++)
+                for (int i = 0; i < parola[indice].length(); i++)
                 {
-                    if (matrix[x][y + i] != empty)
+                    if (matrix[x][y + i] != vuoto)
                     {
-                        x = random() % rows; // row random
-                        y = random() % (columns - words[index].length());
+                        x = random() % righe; // row random
+                        y = random() % (colonne - parola[indice].length());
                         i = 0;
                     }
                 }
  
-                for (int i = 0; i < words[index].length(); i++)
+                for (int i = 0; i < parola[indice].length(); i++)
                 {
-                    matrix[x][y + i] = words[index][i];
-                    // cout << "index: " << index << " i: " << i << " x: " << x << " y: " << y << endl;
-                    // cout << "matrix[" << x << "][" << y + i << "] = " << words[index][i] << endl;
+                    matrix[x][y + i] = parola[indice][i];
+                    // cout << "indice: " << indice << " i: " << i << " x: " << x << " y: " << y << endl;
+                    // cout << "matrix[" << x << "][" << y + i << "] = " << parola[indice][i] << endl;
                     // cout << "result: " << matrix[x][y + i] << endl;
                 }
             }
         }
-        else if (index % 3 == 1)
+        else if (indice % 3 == 1)
         { // verticale
-            if (words[index].length() > rows)
+            if (parola[indice].length() > righe)
             {
-                cout << "word " << words[index] << " is too long" << endl;
+                cout << "word " << parola[indice] << " is too long" << endl;
                 continue;
             }
             else
             {
                 contatore++;
-                int x = random() % (rows - words[index].length());
-                int y = random() % columns;
+                int x = random() % (righe - parola[indice].length());
+                int y = random() % colonne;
  
-                while (matrix[x][y] != empty)
+                while (matrix[x][y] != vuoto)
                 {
-                    x = random() % (rows - words[index].length());
-                    y = random() % columns;
+                    x = random() % (righe - parola[indice].length());
+                    y = random() % colonne;
                 }
  
-                for (int i = 0; i < words[index].length(); i++)
+                for (int i = 0; i < parola[indice].length(); i++)
                 {
-                    if (matrix[x + i][y] != empty)
+                    if (matrix[x + i][y] != vuoto)
                     {
-                        x = random() % (rows - words[index].length());
-                        y = random() % columns;
+                        x = random() % (righe - parola[indice].length());
+                        y = random() % colonne;
                         i = 0;
                     }
                 }
  
-                for (int i = 0; i < words[index].length(); i++)
+                for (int i = 0; i < parola[indice].length(); i++)
                 {
-                    matrix[x + i][y] = words[index][i];
-                    // cout << "index: " << index << " i: " << i << " x: " << x << " y: " << y << endl;
-                    // cout << "matrix[" << x << "][" << y + i << "] = " << words[index][i] << endl;
+                    matrix[x + i][y] = parola[indice][i];
+                    // cout << "indice: " << indice << " i: " << i << " x: " << x << " y: " << y << endl;
+                    // cout << "matrix[" << x << "][" << y + i << "] = " << parola[indice][i] << endl;
                     // cout << "result: " << matrix[x][y + i] << endl;
                 }
             }
@@ -108,12 +108,12 @@ int main()
     }
  
  
-    // // fill the matrix with random char if empty
-    // for (int i = 0; i < rows; i++)
+    // // fill the matrix with random char if vuoto
+    // for (int i = 0; i < righe; i++)
     // {
-    //     for (int j = 0; j < columns; j++)
+    //     for (int j = 0; j < colonne; j++)
     //     {
-    //         if (matrix[i][j] == empty)
+    //         if (matrix[i][j] == vuoto)
     //         {
     //             matrix[i][j] = random() % 25 + 97;
     //         }
@@ -124,7 +124,7 @@ int main()
  
     // print
     cout << "    ";
-    for (int i = 0; i < columns; i++)
+    for (int i = 0; i < colonne; i++)
     {
         if(i < 10){
             cout << " " << i;
@@ -135,7 +135,7 @@ int main()
         
     }
     cout << endl;
-    for (int i = 0; i < rows; i++)
+    for (int i = 0; i < righe; i++)
     {
         
         if(i < 10){
@@ -145,7 +145,7 @@ int main()
             cout << " " << i << " |";
         }
         
-        for (int j = 0; j < columns; j++)
+        for (int j = 0; j < colonne; j++)
         {
             cout << matrix[i][j] << " ";
         }
